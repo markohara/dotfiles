74359fd0591238d7d1a56bd830e908e3b8b3e3ea
Mark O'Hara <markohara98@gmail.com>
2025-01-25 17:55:54 +0000

    chore: remove themeing from fzf setup, leverage terminal theme

--- config/zsh/zsh/tools/fzf.zsh
+++ config/zsh/zsh/tools/fzf.zsh
@@ -7,17 +7,17 @@ zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'ls --color $realpath'
 # fzf config
 eval "$(fzf --zsh)"
 
-bg="#2A2928"
-fg="#CBE0F0"
-bg_highlight="#333231"
-accent="#E6005F"
-blue="#06BCE4"
-cyan="#2CF9ED"
-green="#5AF78E"
+# bg="#2A2928"
+# fg="#CBE0F0"
+# bg_highlight="#333231"
+# accent="#E6005F"
+# blue="#06BCE4"
+# cyan="#2CF9ED"
+# green="#5AF78E"
 
-export FZF_DEFAULT_OPTS="
-  --color=fg:$fg,bg:$bg,hl:$blue
-  --color=fg+:$fg,bg+:$bg_highlight,hl+:$cyan
-  --color=info:$accent,prompt:$green,pointer:$accent
-  --color=marker:$accent,spinner:$cyan,header:$accent
-"
+# export FZF_DEFAULT_OPTS="
+#   --color=fg:$fg,bg:$bg,hl:$blue
+#   --color=fg+:$fg,bg+:$bg_highlight,hl+:$cyan
+#   --color=info:$accent,prompt:$green,pointer:$accent
+#   --color=marker:$accent,spinner:$cyan,header:$accent
+# "
