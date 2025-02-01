8a08e74186ba1e1996202a773fb1664e0b61b30e
Mark O'Hara <markohara98@gmail.com>
2025-02-01 17:47:50 +0000

    feat: add spoon to get intellij style open quickly behaviour with shift shift in xcode

---
+++ config/hammerspoon/.config/hammerspoon/Spoons/OpenQuickly.spoon/init.lua
@@ -0,0 +1,50 @@
+--- === QuickOpen ===
+---
+--- A Hammerspoon Spoon that enables quick open functionality in Xcode using double-tap shift
+
+local obj = {}
+obj.__index = obj
+
+obj.shiftTap = nil
+obj.lastShiftTime = 0
+obj.shiftTapCount = 0
+obj.shiftTimeout = 0.3
+
+function obj:init()
+    self.shiftTap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, function(event)
+        local flags = event:getFlags()
+        if flags.shift then
+            self:handleShiftTap()
+        end
+        return false
+    end)
+    return self
+end
+
+function obj:start()
+    self.shiftTap:start()
+    return self
+end
+
+function obj:handleShiftTap()
+    local currentTime = os.time()
+    
+    if (currentTime - self.lastShiftTime) < self.shiftTimeout then
+        self.shiftTapCount = self.shiftTapCount + 1
+    else
+        self.shiftTapCount = 1
+    end
+    
+    self.lastShiftTime = currentTime
+    
+    if self.shiftTapCount == 2 then
+        local app = hs.application.frontmostApplication()
+        if app:name() == "Xcode" then
+            hs.eventtap.keyStroke({"cmd", "shift"}, "O")
+        end
+        
+        self.shiftTapCount = 0
+    end
+end
+
+return obj
--- config/hammerspoon/.config/hammerspoon/init.lua
+++ config/hammerspoon/.config/hammerspoon/init.lua
@@ -1,6 +1,10 @@
 hs = hs
 
+hs.loadSpoon("OpenQuickly")
+
 -- Bind end key to output #
 hs.hotkey.bind({}, "end", function()
     hs.eventtap.keyStrokes("#")
 end)
+
+spoon.OpenQuickly:start()
