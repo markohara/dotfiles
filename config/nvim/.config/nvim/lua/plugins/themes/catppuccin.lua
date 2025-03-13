23b4c8337ffa2607584e811220749b1cbbdc4d28
Mark O'Hara <markohara98@gmail.com>
2025-03-13 13:31:51 +0000

    wip: trying to align themes across ghostty, neovim, tmux

--- config/nvim/.config/nvim/lua/plugins/themes/catppuccin.lua
+++ config/nvim/.config/nvim/lua/plugins/themes/catppuccin.lua
@@ -2,6 +2,11 @@ return {
     { 
         "catppuccin/nvim",
         name = "catppuccin",
-        priority = 1000
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                transparent_background = true
+            })
+          end,
     }
 }
