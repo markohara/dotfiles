131f898a41c3bdc597e4078a93874047397ed9af
Mark O'Hara <markohara98@gmail.com>
2025-03-17 11:52:43 +0000

    feat: initial intergration of fugitive

--- config/nvim/.config/nvim/lua/plugins/specs/telescope.lua
+++ config/nvim/.config/nvim/lua/plugins/specs/telescope.lua
@@ -25,7 +25,7 @@ return {
         vim.keymap.set("n", "<leader>fh", builtin.help_tags)
         vim.keymap.set("n", "<leader>fd", builtin.git_files)
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-        -- vim.keymap.set("n", "<leader>fd", builtin.find_files)
+        vim.keymap.set("n", "<leader>ff", builtin.find_files)
         -- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set("n", "<leader>en", function()
           builtin.find_files {
