7752a63f0d0fd8e11cf9aeecb6cca219d744938c
Mark O'Hara <markohara98@gmail.com>
2025-03-17 22:58:10 +0000

    feat: add global .gitignore, add additional telescope search scopes

--- config/nvim/.config/nvim/lua/plugins/specs/telescope.lua
+++ config/nvim/.config/nvim/lua/plugins/specs/telescope.lua
@@ -1,43 +1,51 @@
 return {
-    {
-      'nvim-telescope/telescope.nvim',
-      tag = '0.1.8',
-      dependencies = {
-        'nvim-lua/plenary.nvim',
-        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
-      },
-      config = function()
-        require('telescope').setup {
-          pickers = {
-            find_files = {
-              --theme = "ivy",
-              find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' },
-            }
-          },
-          extensions = {
-            fzf = {}
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
+    config = function()
+      require('telescope').setup {
+        pickers = {
+          find_files = {
+            --theme = "ivy",
+            find_command = { 'rg', '--files', '--iglob', '!.git', '--hidden' },
           }
+        },
+        extensions = {
+          fzf = {}
         }
-  
-        require('telescope').load_extension('fzf')
-  
-        local builtin = require('telescope.builtin')
-        vim.keymap.set("n", "<leader>fh", builtin.help_tags)
-        vim.keymap.set("n", "<leader>fd", builtin.git_files)
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-        vim.keymap.set("n", "<leader>ff", builtin.find_files)
-        -- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set("n", "<leader>en", function()
-          builtin.find_files {
-            cwd = vim.fn.stdpath("config")
-          }
-        end)
-        vim.keymap.set("n", "<leader>ep", function()
-          builtin.find_files {
-            cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
-          }
-        end)
-  
-      end
-    }
+      }
+
+      require('telescope').load_extension('fzf')
+
+      local builtin = require('telescope.builtin')
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+      vim.keymap.set("n", "<leader>fd", builtin.git_files)
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+      vim.keymap.set("n", "<leader>fag", function()
+        builtin.live_grep({
+          additional_args = function()
+            return { "--hidden" }
+          end,
+        })
+      end, { desc = "Grep" })
+      vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = 'Telescope live grep' })
+      vim.keymap.set("n", "<leader>ff", builtin.find_files)
+      -- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+      vim.keymap.set("n", "<leader>en", function()
+        builtin.find_files {
+          cwd = vim.fn.stdpath("config")
+        }
+      end)
+      vim.keymap.set("n", "<leader>ep", function()
+        builtin.find_files {
+          cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+        }
+      end)
+
+    end
   }
+}
