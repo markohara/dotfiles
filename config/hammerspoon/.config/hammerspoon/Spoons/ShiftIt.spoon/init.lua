a83d6955bea3ea1cf8c734a03e2b1c52a1b2331d
Mark O'Hara <markohara98@gmail.com>
2025-10-11 19:35:06 +0100

    refactor shiftit, to use default shift key

--- config/hammerspoon/.config/hammerspoon/Spoons/ShiftIt.spoon/init.lua
+++ config/hammerspoon/.config/hammerspoon/Spoons/ShiftIt.spoon/init.lua
@@ -1,10 +1,11 @@
 --- === ShiftIt ===
 ---
 --- A spoon that enables performing a quick action for an application using double-tap shift
+
 -- Function to create a fixed-size array
 function createFixedSizeArray(maxSize)
     local array = {}
-    
+
     function array:add(item)
         if #self < maxSize then
             table.insert(self, item)
@@ -18,9 +19,8 @@ function createFixedSizeArray(maxSize)
         while #self > 0 do
             table.remove(self)
         end
-        print(#self)
     end
-    
+
     return array
 end
 
@@ -28,23 +28,19 @@ local obj = {}
 obj.__index = obj
 
 obj.modTap = nil
-obj.keyTap = nil
 
 obj.lastShiftTime = 0
 obj.hotkeyTapCount = 0
 obj.timeout = 0.3
 
-obj.hotkey = nil
-obj.swallows = false
+obj.hotkey = "shift"
 obj.appMappings = {}
 
 obj.keyStrokes = createFixedSizeArray(10)
 
 function obj:init()
     self.modTap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, function(event)
-        if not self:isAppMapped() then return false end
         local flags = event:getFlags()
-
         for flagName, isActive in pairs(flags) do
             if isActive then
                 self.keyStrokes:add(flagName)
@@ -54,33 +50,15 @@ function obj:init()
         self:handleKeyPressedEvent()
         return false
     end)
-
-    self.keyTap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(event)
-        if not self:isAppMapped() then return false end
-        local key = hs.keycodes.map[event:getKeyCode()]
-
-        self.keyStrokes:add(key)
-        self:handleKeyPressedEvent()
-
-        return self.swallows and key == self.hotkey or false
-    end)
     return self
 end
 
-function obj:isAppMapped()
-    return self.appMappings[hs.application.frontmostApplication():name()] ~= nil
-end
-
-function obj:register(hotkey, swallows, mappings)
-    self.hotkey = hotkey
-    self.swallows = swallows
-
+function obj:register(mappings)
     for appName, config in pairs(mappings) do
         self.appMappings[appName] = {modifiers = config.modifiers, key = config.key}
     end
 
     self.modTap:start()
-    self.keyTap:start()
 end
 
 function obj:handleKeyPressedEvent()
@@ -89,7 +67,7 @@ function obj:handleKeyPressedEvent()
     self.lastShiftTime = currentTime
 
     if self.hotkeyTapCount == 1 then return end
-    
+
     local n = #self.keyStrokes
     if n < 2 then return end
     if self.keyStrokes[n-1] ~= self.hotkey or self.keyStrokes[n] ~= self.hotkey then return end
@@ -98,7 +76,7 @@ function obj:handleKeyPressedEvent()
     if mapping then
         hs.eventtap.keyStroke(mapping.modifiers, mapping.key)
     end
-    
+
     self.hotkeyTapCount = 0
     self.keyStrokes:flush()
 end
--- config/hammerspoon/.config/hammerspoon/init.lua
+++ config/hammerspoon/.config/hammerspoon/init.lua
@@ -7,15 +7,13 @@ local binder = spoon.Binder
 binder:register(
     {
         {{key = "end"}, {key = "#"}},
-        -- {{modifiers = {"shift"}, key = "delete"}, {key = "forwarddelete"}}
+        {{modifiers = {"shift"}, key = "delete"}, {key = "forwarddelete"}}
     }
 )
 
 
 local shiftIt = spoon.ShiftIt
 shiftIt:register(
-    "f9",
-    true,
     {
         Xcode = {modifiers = {"cmd", "shift"}, key = "o"},
         Cursor = {modifiers = {"cmd"}, key = "p"},
