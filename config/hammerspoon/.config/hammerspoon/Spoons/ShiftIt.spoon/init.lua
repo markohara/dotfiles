e979062cb2d77a84b417e8a6e462787bee7d79e9
Mark O'Hara <markohara98@gmail.com>
2025-10-25 21:06:45 +0100

    move to raw flags to distinguish between left and right modifiers

--- config/hammerspoon/.config/hammerspoon/Spoons/ShiftIt.spoon/init.lua
+++ config/hammerspoon/.config/hammerspoon/Spoons/ShiftIt.spoon/init.lua
@@ -4,24 +4,24 @@
 
 -- Function to create a fixed-size array
 function createFixedSizeArray(maxSize)
-    local array = {}
+  local array = {}
 
-    function array:add(item)
-        if #self < maxSize then
-            table.insert(self, item)
-        else
-            table.remove(self, 1)
-            table.insert(self, item)
-        end
+  function array:add(item)
+    if #self < maxSize then
+      table.insert(self, item)
+    else
+      table.remove(self, 1)
+      table.insert(self, item)
     end
+  end
 
-    function array:flush()
-        while #self > 0 do
-            table.remove(self)
-        end
+  function array:flush()
+    while #self > 0 do
+      table.remove(self)
     end
+  end
 
-    return array
+  return array
 end
 
 local obj = {}
@@ -36,59 +36,70 @@ obj.timeout = 0.075
 
 obj.hotkey = "shift"
 obj.appMappings = {}
+local flagMap = {
+  [0x2] = "shift_l",
+  [0x4] = "shift_r",
+  [0x1] = "ctrl_l",
+  [0x2000] = "ctrl_r",
+  [0x20] = "alt_l",
+  [0x40] = "alt_r",
+  [0x8] = "cmd_l",
+  [0x10] = "cmd_r"
+}
 
 obj.keyStrokes = createFixedSizeArray(10)
 
 function obj:init()
-    self.modTap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, function(event)
-        local flags = event:getFlags()
-        for flagName, isActive in pairs(flags) do
-            if isActive then
-                self.keyStrokes:add(flagName)
-            end
-        end
+  self.modTap = hs.eventtap.new({hs.eventtap.event.types.flagsChanged}, function(event)
+    local flags = event:rawFlags()
+    for mask, name in pairs(flagMap) do
+      if flags & mask > 0 then
+        self.keyStrokes:add(name)
+      end
+    end
 
-        self:handleKeyPressedEvent()
-        return false
-    end)
+    self:handleKeyPressedEvent()
 
-    self.keyTap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(event)
-        self.keyStrokes:flush()
-      return false
-    end)
-    return self
+    return false
+  end)
+
+  self.keyTap = hs.eventtap.new({hs.eventtap.event.types.keyDown}, function(event)
+    self.keyStrokes:flush()
+    return false
+  end)
+  return self
 end
 
 function obj:register(mappings)
-    for appName, config in pairs(mappings) do
-        self.appMappings[appName] = {modifiers = config.modifiers, key = config.key}
-    end
+  for appName, config in pairs(mappings) do
+    self.appMappings[appName] = {modifiers = config.modifiers, key = config.key}
+  end
 
-    self.modTap:start()
-    self.keyTap:start()
+  self.modTap:start()
+  self.keyTap:start()
 end
 
 function obj:handleKeyPressedEvent()
-    local currentTime = os.clock()
-    self.hotkeyTapCount = (currentTime - self.lastShiftTime > self.timeout) and 1 or self.hotkeyTapCount + 1
-    self.lastShiftTime = currentTime
+  local currentTime = os.clock()
+  self.hotkeyTapCount = (currentTime - self.lastShiftTime > self.timeout) and 1 or self.hotkeyTapCount + 1
+  self.lastShiftTime = currentTime
 
-    if self.hotkeyTapCount == 1 then return end
+  if self.hotkeyTapCount == 1 then return end
 
-    local n = #self.keyStrokes
-    if n < 2 then return end
-    if self.keyStrokes[n-1] ~= self.hotkey or self.keyStrokes[n] ~= self.hotkey then 
-      self.keyStrokes:flush()
-      return
-    end
-
-    local mapping = self.appMappings[hs.application.frontmostApplication():name()]
-    if mapping then
-        hs.eventtap.keyStroke(mapping.modifiers, mapping.key)
-    end
-
-    self.hotkeyTapCount = 0
+  local n = #self.keyStrokes
+  if n < 2 then return end
+  if self.keyStrokes[n-1] == self.keyStrokes[n] and (not self.keyStrokes[n]:match(self.hotkey)) then
     self.keyStrokes:flush()
+    return
+  end
+
+  local mapping = self.appMappings[hs.application.frontmostApplication():name()]
+  if mapping then
+    hs.eventtap.keyStroke(mapping.modifiers, mapping.key)
+  end
+
+  self.hotkeyTapCount = 0
+  self.keyStrokes:flush()
 end
 
 return obj
