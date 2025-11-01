1f6a3d2fe46d725ac33048330505ad5e844fae71
Mark O'Hara <markohara98@gmail.com>
2025-11-01 15:09:21 +0000

    Trying to replicate fugitive keymaps

--- config/nvim/.config/nvim/lua/plugins/specs/neogit.lua
+++ config/nvim/.config/nvim/lua/plugins/specs/neogit.lua
@@ -3,12 +3,43 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "sindrets/diffview.nvim",
-    "nvim-telescope/telescope.nvim", -- optional
+    "nvim-telescope/telescope.nvim",
   },
   config = function()
-    require('neogit').setup({
+    local neogit = require('neogit')
+
+    neogit.setup({
       kind = "floating"
     })
-    vim.keymap.set("n", "<leader>gs", vim.cmd.Neogit)
+
+    -- Main Neogit status keybinding
+    vim.keymap.set("n", "<leader>gs", vim.cmd.Neogit, { desc = "Open Neogit status" })
+
+    local Mark_Neogit = vim.api.nvim_create_augroup("Mark_Neogit", {})
+
+    vim.api.nvim_create_autocmd("FileType", {
+      group = Mark_Neogit,
+      pattern = "Neogit*",
+      callback = function()
+        if not vim.bo.ft:match("^Neogit") then
+          return
+        end
+
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = { buffer = bufnr, remap = false, desc = "" }
+
+        -- Push to remote
+        vim.keymap.set("n", "<leader>p", function()
+          vim.cmd("Neogit push")
+        end, opts,  { desc = "Git push" })
+
+        -- Pull with rebase
+        vim.keymap.set("n", "<leader>P", function()
+          vim.cmd("Neogit pull")
+        end, opts,  { desc = "Git pull" })
+      end,
+    })
+
   end
 }
+
