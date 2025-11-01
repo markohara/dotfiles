b88681d85bca698c6306e5906116531fa05dacef
Mark O'Hara <markohara98@gmail.com>
2025-11-01 20:28:34 +0000

    add autoparis for {}

---
+++ config/nvim/.config/nvim/lua/plugins/specs/autopairs.lua
@@ -0,0 +1,13 @@
+return {
+  'windwp/nvim-autopairs',
+  event = "InsertEnter",
+  config = function ()
+    require('nvim-autopairs').setup({})
+
+    local npairs = require('nvim-autopairs')
+    npairs.clear_rules()
+
+    local Rule = require('nvim-autopairs.rule')
+    npairs.add_rule(Rule("{", "}"))
+  end
+}
