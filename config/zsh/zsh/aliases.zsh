ce467d0fb05cfd3f23d8b60d3e63977fc3df24d7
Mark O'Hara <markohara98@gmail.com>
2025-11-01 00:23:23 +0000

    add some git aliases

--- config/zsh/zsh/aliases.zsh
+++ config/zsh/zsh/aliases.zsh
@@ -3,3 +3,12 @@ alias cd="z"
 
 alias commit="commitizen"
 alias sesh="tmux-sessionizer"
+
+# Git
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit -am"
+alias gb="git branch"
+alias gp="git pull"
+alias gP="git push"
+alias gd="git diff"
